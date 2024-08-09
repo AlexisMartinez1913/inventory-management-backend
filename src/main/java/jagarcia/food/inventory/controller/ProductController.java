4768e1cba1c7bae3b3a73ea98c8ec0256050e134
Alexis Martinez <jhonyalexis100@gmail.com>
2024-08-09 14:38:12 -0500

    Build add Product Rest Api

--- src/main/java/jagarcia/food/inventory/controller/ProductController.java
+++ src/main/java/jagarcia/food/inventory/controller/ProductController.java
@@ -5,10 +5,7 @@ import jagarcia.food.inventory.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,10 @@ public class ProductController {
         var products = productService.getAllPoducts();
         products.forEach((product -> logger.info(product.toString())));
         return products;
+    }
+    @PostMapping("/products")
+    public Product saveProduct(@RequestBody Product product) {
+        logger.info("Product to save: " + product);
+        return productService.saveProduct(product);
     }
 }
